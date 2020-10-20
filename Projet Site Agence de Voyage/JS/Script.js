0c400083b498ee1590d00632153a1682cca8e8b9
Chloe BAILLIVET <chloe.baillivet@cpe.fr>
2020-10-20 11:38:08 +0200

    add destinationcachées

--- Projet Site Agence de Voyage/JS/Script.js
+++ Projet Site Agence de Voyage/JS/Script.js
@@ -1,37 +1,49 @@
-function CalculDuPrix(){
+function CalculDuPrix() {
     DA = new Date(document.getElementById('Date-d-arrivee').value).getTime()
-    
+
     DD = new Date(document.getElementById('Date-de-depart').value).getTime()
     let nbjour =
-    jour(DA,DD)
+        jour(DA, DD)
     let PrixParJour =
-    CalculPrixParJour(document.getElementById('Nb-Adulte').value,
-    document.getElementById('Nb-Enfant').value,
-    document.getElementById('Petit-dejeunerO').checked)
-    return(document.getElementById('resultat').innerHTML=(nbjour*PrixParJour))
+        CalculPrixParJour(document.getElementById('Nb-Adulte').value,
+            document.getElementById('Nb-Enfant').value,
+            document.getElementById('Petit-dejeunerO').checked)
+    return (document.getElementById('resultat').innerHTML = (nbjour * PrixParJour))
 }
 
-function jour(DateA,DateD){
-   return((DD-DA)/86400000)
+function jour(DateA, DateD) {
+    return ((DD - DA) / 86400000)
 }
 
-function CalculPrixParJour(NbA,NbE,Petitdej){
+function CalculPrixParJour(NbA, NbE, Petitdej) {
     console.log(Petitdej)
     if (Petitdej) {
-        let prixparA=
-        NbA*90
-        let prixparE=
-        NbE*70
-       
-        return (prixparA+prixparE)
+        let prixparA =
+            NbA * 90
+        let prixparE =
+            NbE * 70
+
+        return (prixparA + prixparE)
     }
     else {
-        let prixparA=
-        NbA*85
-        let prixparE=
-        NbE*65 
+        let prixparA =
+            NbA * 85
+        let prixparE =
+            NbE * 65
         console.log(prixparA)
-        return (prixparA+prixparE)
+        return (prixparA + prixparE)
     }
-}
+};
 
+window.onload = function () {
+    var tableaudestination = ["Paris", "Rome", "Sydney", "Wellington", "Safari", "Arctique"];
+    for (var i = 0; i < tableaudestination.length; i++) {
+        console.log(tableaudestination[i])
+        var Cells = document.getElementById(tableaudestination[i]).cells
+        for (var j = 0; j < Cells.length; j++) {
+            if (Cells[j] == "True") {
+                console.log(tableaudestination[i])
+            }
+        }
+    }
+};
