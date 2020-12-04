345461ca4b6714257882c07f1eaeadb7d39df372
Chloe-Baillivet <72148494+Chloe-Baillivet@users.noreply.github.com>
2020-12-04 14:59:03 +0100

    changed script

--- Projet Site Agence de Voyage/JS/Script.js
+++ Projet Site Agence de Voyage/JS/Script.js
@@ -35,61 +35,9 @@ function CalculPrixParJour(NbA, NbE, Petitdej) {
     }
 };
 
-window.onload = function () {
-    var tableaudestination = ["Paris", "Rome", "Sydney", "Wellington", "Safari", "Arctique"];
-    var reftable = document.getElementById("favoris")
-    let indicefav = []
-    for (var i = 0; i < tableaudestination.length; i++) {
-        var tableau =document.getElementById(tableaudestination[i])
-        var Ligne = document.getElementById(tableaudestination[i]).cells
-        for (var j = 0; j < Ligne.length; j++) {
-            if ((Ligne[j].className == "favori") && (Ligne[j].textContent == "True")) {
-                indicefav.push(i)
-            }
-        }
-        
-   
-    
-                //nouvelleLigne = reftable.insertRow();
-                //var nbLignes = reftable.rows.length;
-                //for(var k=0; k < Ligne.length; k++){
-                    //var cell = document.createElement("td");
-                    //var cellText = document.createTextNode("cell in row "+i+", column "+j);
-                    //cell.appendChild(cellText);
-                    //row.appendChild(cell);
-                    //cell = nouvelleLigne.insertCell(0); // création et insertion de la cellule 0
-                    //cell.innerHTML = tableau[i][j].cell;
-                    //console.log(tableau[i][j]);
-            
-        
-    }
-    generate_table(indicefav,document.getElementById(table1))
-};
-function generate_table(i,tableau) {
-    // get the reference for the body
-    NbLignes = i.length
-    NbColonnes = tableau[0].length
-    var body = document.getElementsByTagName("body")[0];
-    // creates a <table> element and a <tbody> element
-    var tbl = document.createElement("table");
-    var tblBody = document.createElement("tbody");
-   
-    // creating all cells
-    for (var i = 0; i < NbLignes; i++) {
-      // creates a table row
-      var row = document.createElement("tr");
-   
-      for (var j = 0; j < NbColonnes; j++) {
-        // Create a <td> element and a text node, make the text
-        // node the contents of the <td>, and put the <td> at
-        // the end of the table row
-        var cell = document.createElement("td");
-        var cellText = document.createTextNode(tableau[i][j]);
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-      }
-   
-      // add the row to the end of the table body
-      tblBody.appendChild(row);
-    }
-}
+TableauDesDestination = [["Paris","Voici la description pour Paris",85,"Temp",False,"Images/Paris.png"],
+["Rome","Voici la description pour Rome",95,"Temp",False,"Images/Rome.png"],
+["Sydney","Voici la description pour Sydney",100,"Temp",True,"Images/Sydney.png"],
+["Wellington","Voici la description pour Paris",95,"Temp",False,"Images/Wellington.png"],
+["Safari","Voici la description pour un Safari",110,"Temp",True,"Images/Safari.png"],
+["Arctique","Voici la description pour un voyage vers l'Arctique",150,"Temp",True,"Images/Nord.png"]];
